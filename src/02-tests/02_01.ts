32d63375efee75af486f2eed9189cc36a15efea5
Tampoka <goal.thanks@gmail.com>
2021-08-12 16:57:23 +0900

    finished student object typization

---
+++ src/02-tests/02_01.ts
@@ -0,0 +1,50 @@
+type TechType = {
+    id: number
+    title: string
+}
+type CityType = {
+    title: string
+    countryTitle: string
+}
+type AddressType = {
+    streetTitle: string
+    city: CityType
+}
+type StudentType = {
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const student: StudentType = {
+    name: "Bob",
+    age: 34,
+    isActive: false,
+    address: {
+        streetTitle: "Vesennya",
+        city: {
+            title: "London",
+            countryTitle: "Great Britain"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        }, {
+            id: 3,
+            title: "JS"
+        }
+    ]
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[0].title)
