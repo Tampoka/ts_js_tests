2cf91801e55bcf0bfabd653cbf2aafd66934d268
Tampoka <goal.thanks@gmail.com>
2021-08-12 21:35:15 +0900

    created function makeStudentActive+test

--- src/03-tests/03.test.ts
+++ src/03-tests/03.test.ts
@@ -1,5 +1,5 @@
 import {StudentType} from "../02-tests/02_01";
-import {addSkill} from "./03_01";
+import {addSkill, makeStudentActive} from "./03_01";
 
 let student:StudentType
 beforeEach(()=>{
@@ -36,5 +36,10 @@ test("new tech skill should be added to student", ()=>{
     expect(student.technologies.length).toBe(4)
     expect(student.technologies[3].title).toBe("JS")
     expect(student.technologies[3].id).toBeDefined()
+})
 
+test("student should be made active",()=>{
+    expect(student.isActive).toBe(false)
+    makeStudentActive(student)
+    expect(student.isActive).toBe(true)
 })
--- src/03-tests/03_01.ts
+++ src/03-tests/03_01.ts
@@ -10,3 +10,7 @@ export const addSkill=(st: StudentType, skill:string)=>{
 st.technologies.push({id:new Date().getTime(),
     title:skill})
 }
+
+export const makeStudentActive=(s:StudentType)=>{
+    s.isActive=true
+}
