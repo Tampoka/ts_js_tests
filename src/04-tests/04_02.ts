809d58b51131d2f0ce98816f5ea3a2ec0cca8b55
Tampoka <goal.thanks@gmail.com>
2021-08-13 17:13:18 +0900

    changed return with parameter

--- src/04-tests/04_02.ts
+++ src/04-tests/04_02.ts
@@ -4,6 +4,6 @@ export const demolishHousesOnTheStreet= (city: CityType, streetName: string)=>{
     city.houses=city.houses.filter(house=>house.address.street.title!==streetName)
 }
 
-export const getBuildingsWithStaffCountGreaterThan= (buildings: Array<GovernmentBuildingsType>, staffCount: number)=>{
-return buildings.filter(building=>building.staffCount>150)
+export const getBuildingsWithStaffCountGreaterThan= (buildings: Array<GovernmentBuildingsType>, staff: number)=>{
+return buildings.filter(building=>building.staffCount>staff)
 }
