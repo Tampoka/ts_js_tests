a96a5f82ace1ff0c557455ce3ec34a5c36434b1d
Tampoka <goal.thanks@gmail.com>
2021-08-27 16:31:08 +0900

    created test for messages

---
+++ src/05-tests/05_01.test.ts
@@ -0,0 +1,21 @@
+import {createGreetingMessage, ManType} from "./05_01";
+
+let people:Array<ManType>=[]
+
+beforeEach(()=>{
+    people = [
+        {name: "Bob Smart", age: 33},
+        {name: "John Dow", age: 52},
+        {name: "Ann Dell", age: 27}
+    ]
+})
+
+test("should get array of greeting messages",()=>{
+    const messages=createGreetingMessage(people)
+
+    expect(messages.length).toBe(3);
+    expect(messages[0]).toBe("Hello, Bob ! Welcome to IT-Incubator.")
+    expect(messages[0]).toBe("Hello, John ! Welcome to IT-Incubator.")
+    expect(messages[0]).toBe("Hello, Ann ! Welcome to IT-Incubator.")
+
+})
