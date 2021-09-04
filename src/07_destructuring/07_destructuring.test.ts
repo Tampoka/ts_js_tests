e2d9c1b323b9033d8d9113b1869752baf13df20d
Tampoka <goal.thanks@gmail.com>
2021-09-04 17:14:18 +0900

    corrected length

--- src/07_destructuring/07_destructuring.test.ts
+++ src/07_destructuring/07_destructuring.test.ts
@@ -5,7 +5,7 @@ beforeEach(() => {
     props = {
         name: "John",
         age: 34,
-        lessons: [{title: "1"}, {title: "2"}],
+        lessons: [{title: "1"}, {title: "2"},{title: "3"}],
         address: {
             street: {
                 title: "Main st."
@@ -31,18 +31,20 @@ test("destructuring object", () => {
     expect(lessons.length).toBe(2)
 
     expect(a).toBe(34)
-    expect(l.length).toBe(2)
+    expect(l.length).toBe(3)
 })
 
 test("destructuring array", () => {
     const l1=props.lessons[0]
     const l2=props.lessons[1]
 
-    const [ls1,ls2]=props.lessons
+    const [,ls2]=props.lessons
+    const [les1,...restLessons]=props.lessons
 
     expect(l1.title).toBe("1")
     expect(l2.title).toBe("2")
-    expect(ls1.title).toBe("1")
+    // expect(ls1.title).toBe("1")
     expect(ls2.title).toBe("2")
+    expect(restLessons.length).toBe(2)
 
 })
