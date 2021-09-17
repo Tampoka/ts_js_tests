12b0322c03487a28b41a731559cc9bc3981e0997
Tampoka <goal.thanks@gmail.com>
2021-09-17 12:41:50 +0900

    Object(keys),Object (values)

---
+++ src/08_associative array/08.js
@@ -0,0 +1,37 @@
+let user={
+    name:"John",
+    age:49,
+    address:{
+        city:{
+         title:"London"
+        }
+    }
+}
+
+const streetName=user.address.city.title
+const streetName2=user["address"]["city"]["title"]
+
+let city={}
+city.title="Paris"
+city["citizensCount"]=100
+
+let users=["John","Bob","Ann"]
+// let firstUser=users[0]
+let firstUser=users["0"]
+users["map"]((el)=>el.toUpperCase())
+
+let usersObj={
+    "0":"John",
+    "1":"Bob",
+    "2":"Ann"
+}
+
+let myUser=usersObj[0]
+usersObj["0"]="Super Mario"
+
+usersObj["table"]="wood"
+usersObj["hello, how are you?"]="fine!"
+
+Object.keys(usersObj)
+Object.values(usersObj)
+
